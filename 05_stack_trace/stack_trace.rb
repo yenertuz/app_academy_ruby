3b2a61271f7e368466d2b4769cc0ee4210431773
Yener Tüz <yenertuz@Yeners-iMac.local>
2018-12-07 19:36:44 -0800

    stack_trace

---
+++ 05_stack_trace/stack_trace.rb
@@ -0,0 +1,7 @@
+MAX_STACK_SIZE = 200
+tracer = proc do |event|
+  if event == 'call' && caller_locations.length > MAX_STACK_SIZE
+    fail "Probable Stack Overflow"
+  end
+end
+set_trace_func(tracer)
